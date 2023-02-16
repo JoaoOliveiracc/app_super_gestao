3e5f45cb00f3aad471a461d2259e62f76d71e1fa
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2023-02-16 09:20:16 -0300

    Nomeando rotas e usando o route

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'IndexController@index');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/login', function() {return 'Login';});
+Route::get('/', 'IndexController@index')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function() {return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function() {return 'Clientes';});
-    Route::get('/fornecedores', function() {return 'Fornecedores';});
-    Route::get('/produtos', function() {return 'Produtos';});
+    Route::get('/clientes', function() {return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function() {return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function() {return 'Produtos';})->name('app.produtos');
 });
