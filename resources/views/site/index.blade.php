241ad95c77bc8415d0fe4c65a9d6dc638a4d7bc0
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2023-02-16 08:48:32 -0300

    Menu basic de nav

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,13 @@
-<h3>Index (View)</h3>
+<h3>Home (View)</h3>
+
+<ul>
+    <li>
+        <a href="/">Home</a>
+    </li>
+    <li>
+        <a href="/sobre-nos">Sobre nós</a>
+    </li>
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,13 @@
 <h3>Sobre nós (View)</h3>
+
+<ul>
+    <li>
+        <a href="/">Home</a>
+    </li>
+    <li>
+        <a href="/sobre-nos">Sobre nós</a>
+    </li>
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
