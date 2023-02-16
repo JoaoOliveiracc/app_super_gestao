2a96b774c91500a29d4feb9deed0bc2aaafbbece
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2023-02-16 08:39:37 -0300

    Parâmetros e por rotas e exp regulares

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', 'IndexController@index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
+
+Route::get('/contato/{nome}/{categoria_id}', function(
+    string $nome = 'Desconhecido',
+    int $categoria_id = 1 // 1 - Informação
+) {
+    echo "Estamos aqui: $nome - $categoria_id";
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
